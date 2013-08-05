6cf3cc752949b4c39991258e6cb6a782f75c4515
Myron Wu <myronwu@gmail.com>
2013-08-05 16:22:38 -0700

    Adding mlton port

---
+++ lang/mlton/files/patch-InclGitFile.py
@@ -0,0 +1,8 @@
+--- doc/guide/bin/InclGitFile.py.orig	2013-08-05 15:29:14.000000000 -0700
++++ doc/guide/bin/InclGitFile.py	2013-08-05 15:27:26.000000000 -0700
+@@ -1,4 +1,4 @@
+-#!/usr/bin/env python
++#!/usr/bin/env python2.7
+ 
+ import sys
+ import urllib
